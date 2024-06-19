e0c6d5d36a22a4741e2114568c7364cbb0d5dc7a
Vishnu <vishnuravi2311@gmail.com>
2024-06-19 19:36:20 +0530

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ import CVCPage from "./components/CVCPage/CVCPage";
 import PastResults from "./components/PastResults/PastResults";
 import Predictions from "./components/PredictionPage/Predictions";
 
-export const URL = 'https://t20wc-api.onrender.com';
+// export const URL = 'https://t20wc-api.onrender.com';
+export const URL = process.env.REACT_APP_SERVER_URL;
 // export const URL = "http://localhost:5000";
 
 function App() {
